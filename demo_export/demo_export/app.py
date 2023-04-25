920d2a8fc0e2b1162334e05d2080d4c870dcef86
Dmytro Parasochka <dmytro.parasochka@avenga.com>
2023-04-25 10:01:38 +0300

    remove SF connector

--- demo_export/demo_export/app.py
+++ demo_export/demo_export/app.py
@@ -3,12 +3,10 @@ import json
 
 from database import Database
 from excel import Excel
-from salesforceConnector import SalesforceConnector
 
 def lambda_handler(event, context):
     records = Database.get_records()
     excel_file = Excel.create_file(records)
-    # SalesforceConnector.upload_file(excel_file) ??? TODO
 
     base64_data = base64.b64encode(excel_file).decode()
     return {
--- demo_export/demo_export/salesforceConnector.py
+++
@@ -1,3 +0,0 @@
-class SalesforceConnector:
-    def upload_file():
-        pass
