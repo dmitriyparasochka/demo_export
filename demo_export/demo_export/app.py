2ec84883c8f16a02238108fdb63ae4ed29a302fa
Dmytro Parasochka <dmytro.parasochka@avenga.com>
2023-04-25 10:02:37 +0300

    fixed indentation

--- demo_export/demo_export/app.py
+++ demo_export/demo_export/app.py
@@ -5,11 +5,11 @@ from database import Database
 from excel import Excel
 
 def lambda_handler(event, context):
-    records = Database.get_records()
-    excel_file = Excel.create_file(records)
-
-    base64_data = base64.b64encode(excel_file).decode()
-    return {
-        'statusCode': 200,
-        'body': json.dumps({'file': base64_data})
-    }
+  records = Database.get_records()
+  excel_file = Excel.create_file(records)
+  base64_data = base64.b64encode(excel_file).decode()
+  
+  return {
+      'statusCode': 200,
+      'body': json.dumps({'file': base64_data})
+  }
